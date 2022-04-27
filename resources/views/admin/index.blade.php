0103ab2eef71615275c51b4813d212e603aa6dc3
NA0HARU <burakhandogan2001@hotmail.com>
2022-04-27 15:35:42 +0300

    5th commit
    vid 7

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.adminbase')
+
+@section('title', 'Admin Panel')
+
+
+@section('content')
+    <!-- Start Page Content -->
+    <!-- ============================================================== -->
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    This is some text within a card block.
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- End PAge Content -->
+    <!-- ============================================================== -->
+
+@endsection
